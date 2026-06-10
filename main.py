b5cf4beff430498ff9c05fbf733ea9928cc217ee
MathisSinet <mathis.sinet@gmail.com>
2026-06-10 22:10:38 +0200

    Mini script de test

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from generateurJSON import run_simulator
+from spark_core import spark_core
+from threading import Thread
+from multiprocessing import Process
+from time import sleep
 
 
 def main():
-    run_simulator()
+    generator = Process(target = run_simulator)
+    spark = Thread(target = spark_core, args=(30,))
+
+    generator.start()
+    spark.start()
+    
+    spark.join()
+    generator.terminate()
+
+    print("END")
 
 
 if __name__ == "__main__":
---
+++ spark_core.py
@@ -0,0 +1,69 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.streaming.query import StreamingQuery
+from pyspark.sql.types import StructType, StructField, TimestampType, StringType, DoubleType
+import pyspark.sql.functions as F
+from threading import Thread
+import time
+
+OUTPUT_DIR = "streaming_data"
+
+SCHEMA = StructType([
+    StructField("timestamp", TimestampType(), False),
+    StructField("user_id", StringType(), False),
+    StructField("user_city", StringType(), False),
+    StructField("product_id", StringType(), False),
+    StructField("product_cat", StringType(), False),
+    StructField("seller_id", StringType(), False),
+    StructField("action_type", StringType(), False),
+    StructField("price", DoubleType(), False)
+])
+
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder
+        .appName("SparkProjectG2")
+        .master("local[*]")
+        .config("spark.executor.memory", "2g")
+        .config("spark.driver.memory", "1g")
+        .config("spark.sql.shuffle.partitions", "8")
+        .config("spark.default.parallelism", "8")
+        .getOrCreate()
+)
+
+df = (
+    spark.readStream
+        .schema(SCHEMA)
+        .json(OUTPUT_DIR)
+)
+
+agg = (
+    df
+    .withWatermark("timestamp", "10 minutes")
+    .filter(df.action_type == "ACHAT")
+    .groupBy(
+        F.window("timestamp", "5 minutes")
+    )
+    .count()
+)
+
+
+writer = (
+    agg.writeStream
+        .trigger(processingTime="5 seconds")
+        .outputMode("update")
+        .format("console")
+)
+
+def stop_query_after(delay: float, query: StreamingQuery):
+    time.sleep(delay)
+    query.stop()
+
+def spark_core(delay: float | None = None):
+    query = writer.start()
+    if delay is not None:
+        Thread(target=stop_query_after, args = (30, query)).start()
+    query.awaitTermination()
+
+if __name__ == "__main__":
+    spark_core()
