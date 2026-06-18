40273fde4fa97ec3e812bfaef77f48fd2a89e497
anaelle9 <anaellejoachim05@gmail.com>
2026-06-18 11:41:37 +0200

    Mise a jour du GraphFrame

--- spark_core.py
+++ spark_core.py
@@ -1,3 +1,5 @@
+
+
 from pyspark.sql import SparkSession
 from pyspark.sql.streaming.query import StreamingQuery
 from pyspark.sql.types import StructType, StructField, TimestampType, StringType, DoubleType
@@ -12,6 +14,7 @@ import json
 OUTPUT_DIR = "streaming_data"
 JSON_OUTPUT_PATH = "graph_data.json"
 
+'''
 
 SCHEMA = StructType([
     StructField("timestamp", TimestampType(), False),
@@ -130,3 +133,126 @@ def spark_core(delay: float | None = None):
 
 if __name__ == "__main__":
     spark_core()
+
+'''
+
+
+
+
+
+global_vertices = None
+global_edges = None
+
+SCHEMA = StructType([
+    StructField("timestamp", TimestampType(), False),
+    StructField("user_id", StringType(), False),
+    StructField("user_city", StringType(), False),
+    StructField("product_id", StringType(), False),
+    StructField("product_cat", StringType(), False),
+    StructField("seller_id", StringType(), False),
+    StructField("action_type", StringType(), False),
+    StructField("price", DoubleType(), False)
+])
+
+spark = (
+    SparkSession.builder
+        .appName("SparkProjectG2_StatefulGraph")
+        .master("local[*]")
+        .config("spark.jars.packages", "graphframes:graphframes:0.8.4-spark3.5-s_2.13")
+        .config("spark.executor.memory", "2g")
+        .config("spark.driver.memory", "2g")
+        .config("spark.sql.shuffle.partitions", "2")
+        .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("WARN")
+spark.sparkContext.setCheckpointDir("checkpoint_dir")
+
+df = (
+    spark.readStream
+        .schema(SCHEMA)
+        .json(OUTPUT_DIR)
+)
+
+
+def process_batch_graph(batch_df, batch_id):
+    global JSON_OUTPUT_PATH, global_vertices, global_edges
+
+    if batch_df.isEmpty():
+        return
+
+    print(f"\n--- [Batch {batch_id}] Mise à jour Incrémentale du GraphFrame ---")
+
+    users = batch_df.select(F.col("user_id").alias("id"), F.col("user_id").alias("label")).distinct().withColumn("type", F.lit("user"))
+    products = batch_df.select(F.col("product_id").alias("id"), F.col("product_id").alias("label")).distinct().withColumn("type", F.lit("product"))
+    sellers = batch_df.select(F.col("seller_id").alias("id"), F.col("seller_id").alias("label")).distinct().withColumn("type", F.lit("seller"))
+    
+    new_vertices = users.union(products).union(sellers).distinct()
+
+    edges_user_prod = batch_df.select(F.col("user_id").alias("src"), F.col("product_id").alias("dst"), F.col("action_type").alias("action"))
+    edges_seller_prod = batch_df.select(F.col("seller_id").alias("src"), F.col("product_id").alias("dst")).withColumn("action", F.lit("PROPOSE"))
+    
+    new_edges = edges_user_prod.union(edges_seller_prod).distinct()
+
+
+    if global_vertices is None or global_edges is None:
+        global_vertices = new_vertices
+        global_edges = new_edges
+    else:
+        global_vertices = global_vertices.union(new_vertices).distinct()
+        global_edges = global_edges.union(new_edges).distinct()
+
+
+    global_vertices.cache()
+    global_edges.cache()
+
+    g = GraphFrame(global_vertices, global_edges)
+    
+    degrees_df = g.degrees
+    vertices_enriched = g.vertices.join(degrees_df, "id", "left").fillna(0, subset=["degree"])
+
+    local_vertices = vertices_enriched.collect()
+    local_edges = g.edges.collect()
+
+    cytoscape_elements = []
+    
+    for row in local_vertices:
+        dynamique_size = 45 + (row['degree'] * 3)
+        cytoscape_elements.append({
+            'data': {
+                'id': row['id'], 
+                'label': f"{row['label']} (Global Deg: {row['degree']})",
+                'size': dynamique_size
+            },
+            'classes': row['type']
+        })
+
+    for row in local_edges:
+        cytoscape_elements.append({
+            'data': {'source': row['src'], 'target': row['dst'], 'action': row['action']}
+        })
+
+    with open(JSON_OUTPUT_PATH, "w", encoding="utf-8") as f:
+        json.dump(cytoscape_elements, f, ensure_ascii=False, indent=4)
+        
+    print(f"-> Graphe Global mis à jour : {len(local_vertices)} nœuds et {len(local_edges)} arêtes stockés en RAM.")
+
+writer = (
+    df
+    .withWatermark("timestamp", "10 minutes")
+    .writeStream
+    .trigger(processingTime="5 seconds")
+    .foreachBatch(process_batch_graph)
+    .outputMode("update")
+)
+
+def stop_query_after(delay: float, query: StreamingQuery):
+    time.sleep(delay)
+    query.stop()
+
+def spark_core(delay: float | None = None):
+    query = writer.start()
+    if delay:
+        t = Thread(target=stop_query_after, args=(delay, query))
+        t.start()
+    query.awaitTermination()
